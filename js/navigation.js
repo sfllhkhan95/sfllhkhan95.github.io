6dd8b9377e3105984e587b21795c237537d6a27d
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-09-21 23:53:29 +0500

    Navigation fixes

--- js/navigation.js
+++ js/navigation.js
@@ -1,8 +1,51 @@
+function openDrawer() {
+    $("#sidebar-wrapper").addClass("opened");
+    $("#action-bar").addClass("opened");
+
+    $("#menu-toggle").css("display", "none");
+    $("#menu-close").css("display", "block");
+}
+
+function closeDrawer() {
+    $("#sidebar-wrapper").removeClass("opened");
+    $("#action-bar").removeClass("opened");
+
+    $("#menu-close").css("display", "none");
+    $("#menu-toggle").css("display", "block");
+}
+
 // Toggles the sidebar menu
-$("#menu-toggle, #menu-close").click(function (e) {
+$("#menu-toggle").click(function (e) {
     e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("opened");
-    $("#action-bar").toggleClass("opened");
+    openDrawer();
+});
+$("#menu-close").click(function (e) {
+    e.preventDefault();
+    closeDrawer();
+});
+
+// Scrolls to the selected menu item on the page, and closes sidebar
+$(function () {
+    $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+            location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                // Scroll to correct section
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 0);
+
+
+                document.location.hash = this.hash.slice(1);
+
+                // Close sidebar
+                closeDrawer();
+                return false;
+            }
+        }
+    });
 });
 
 // Sidebar closes when window is resized
