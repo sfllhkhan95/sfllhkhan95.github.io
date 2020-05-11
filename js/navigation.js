e8d11ef66d36a71584ff14f2924ca4cb5ad4a228
Muhammad Saif Ullah Khan <saifkhichi96@gmail.com>
2020-05-11 08:20:56 +0200

    Optimized third-party api acceses by removing Bootstrap & FontAwesome.

---
+++ js/navigation.js
@@ -0,0 +1,25 @@
+/**
+ * @file Manages the main navigation for the web app.
+ * @author saifkhichi96
+ */
+'use strict';
+
+
+ $(function() {
+   console.log("Setting up site navigation...");
+   // Instantiate toolbar
+   const topAppBarEl = document.querySelector('.mdc-top-app-bar');
+   const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
+
+   // Instantiate navigation drawer
+   const drawerEl = document.querySelector('.mdc-drawer');
+   const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
+
+   // Attach toolbar to navigation drawer
+   topAppBar.setScrollTarget(document.querySelector('.main-content'));
+   topAppBar.listen('MDCTopAppBar:nav', () => {
+     drawer.open = !drawer.open;
+   });
+
+   console.log("Navigation setup complete!");
+ });
