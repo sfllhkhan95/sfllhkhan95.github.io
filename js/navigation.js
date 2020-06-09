69eba33465199bdf456c314b2cd68a8e5b4b7b0a
Muhammad Saif Ullah Khan <saifkhichi96@gmail.com>
2020-06-10 00:58:33 +0200

    Fixed webapp navigation.

--- js/navigation.js
+++ js/navigation.js
@@ -5,21 +5,29 @@
 'use strict';
 
 
- $(function() {
-   console.log("Setting up site navigation...");
-   // Instantiate toolbar
-   const topAppBarEl = document.querySelector('.mdc-top-app-bar');
-   const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
+$(function() {
+ console.log("Setting up site navigation...");
+ // Instantiate toolbar
+ const topAppBarEl = document.querySelector('.mdc-top-app-bar');
+ const topAppBar = new mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarEl);
 
-   // Instantiate navigation drawer
-   const drawerEl = document.querySelector('.mdc-drawer');
-   const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
+ // Instantiate navigation drawer
+ const drawerEl = document.querySelector('.mdc-drawer');
+ const drawer = new mdc.drawer.MDCDrawer.attachTo(drawerEl);
 
-   // Attach toolbar to navigation drawer
-   topAppBar.setScrollTarget(document.querySelector('.main-content'));
-   topAppBar.listen('MDCTopAppBar:nav', () => {
-     drawer.open = !drawer.open;
-   });
-
-   console.log("Navigation setup complete!");
+ // Attach toolbar to navigation drawer
+ topAppBar.setScrollTarget(document.querySelector('.main-content'));
+ topAppBar.listen('MDCTopAppBar:nav', () => {
+   drawer.open = !drawer.open;
  });
+
+ console.log("Navigation setup complete!");
+});
+
+function back() {
+  if (document.referrer.indexOf(location.protocol + "//" + location.host) === 0) {
+    history.back();
+  } else {
+    window.location = '/';
+  }
+}
