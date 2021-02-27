9d3d0c5354fa97e711ff938cb95bb809e309a1cf
Saif Khan <saifkhichi96@gmail.com>
2021-02-27 13:50:09 +0100

    updated Firebase 8.2.4 -> 8.2.9

--- assets/scripts/firebase.js
+++ assets/scripts/firebase.js
@@ -1,15 +1,13 @@
 /**
- * @file Imports and initializes the Firebase services.
+ * @file Initializes the Firebase app.
  * @author saifkhichi96
  */
 'use strict';
 
 
-/** Initializes web app with Firebase configuration and
-starts the required services (such as Analytics). */
-function init() {
-  // The web app's Firebase configuration
-  var firebaseConfig = {
+$.getScript("https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js", function() {
+  console.log("Initializing Firebase app...");
+  firebase.initializeApp({
     apiKey: "AIzaSyD-KEP7A1eVAzYaNDahzOUqeCLJaMK4lfk",
     authDomain: "meet-saifkhichi96.firebaseapp.com",
     databaseURL: "https://meet-saifkhichi96.firebaseio.com",
@@ -18,19 +16,12 @@ function init() {
     messagingSenderId: "727257041805",
     appId: "1:727257041805:web:7049b6b5f6abba5ed4cbc4",
     measurementId: "G-Z2P8EMEFG7"
-  };
+  });
 
-  // Initialize Firebase
-  console.log("Initializing Firebase app...");
-  firebase.initializeApp(firebaseConfig);
-
-  // Enable Firebase Analytics
   console.log("Enabling Firebase Analytics...");
-  firebase.analytics();
+  $.getScript("https://www.gstatic.com/firebasejs/8.2.9/firebase-analytics.js", function() {
+    firebase.analytics();
+  });
 
-  console.log("Firebase setup complete!");
-}
-
-
-// Import Firebase services and start initialization
-$(init);
+  $.getScript("https://www.gstatic.com/firebasejs/8.2.9/firebase-storage.js");
+});
